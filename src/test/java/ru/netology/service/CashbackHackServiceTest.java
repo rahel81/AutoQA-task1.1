2b53feb5cb5f1fce98aeb57000b0ce62cb308e7d
Rahel81 <rahimova81@gmail.com>
2021-03-03 16:32:21 +0300

    added CashbackHackServiceTest

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,33 +1,33 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldBoundaryAmount() {
         int actual = service.remain(1000);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCashBackUnderBoundary() {
         int actual = service.remain(900);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCashBackOverBoundary() {
         int actual = service.remain(1200);
         int expected = 800;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
